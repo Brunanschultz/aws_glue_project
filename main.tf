9bb10ac1ee80449f4c07822309656d7f5ddc6b44
Bruna-Schultz <bruna.schultz@neoenergia.com>
2026-06-15 11:48:50 -0300

    Add Readme

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.28"
     }
   }
 }
@@ -12,29 +12,30 @@ terraform {
 # 2. Configure the AWS Provider block
 provider "aws" {
   region = "us-east-1"
+  profile = var.aws_profile
 }
 
+########### S3 BUCKET ###########
 variable "bucket_name" {
   description = "The name of bucket"
   type = string
-  default = "learning-aws-etl-bruna-novais-2026"
+  default = ""
 }
 
+variable "aws_profile" {
+  description = "The AWS profile to use for authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "notification_email" {
+  description = "The email for SNS topic subscription"
+  type        = string
+  sensitive   = true
+}
 
 resource "aws_s3_bucket" "learning-aws-etl" {
   bucket = var.bucket_name
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-
-resource "aws_s3_object" "extract" {
-    bucket = var.bucket_name
-    key    = "extract/wc_top_scorers.csv"
-    source = "${path.module}/data/wc_top_scorers.csv"
 }
 
 
@@ -45,7 +46,6 @@ data "archive_file" "lambda_zip" {
   output_path = "${path.module}/lambda.zip"
 }
 
-# Role da Lambda
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-execution-role"
 
@@ -63,13 +63,10 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# Permissão para escrever logs no CloudWatch
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-
 
 resource "aws_lambda_function" "activate-s3" {
   function_name = "activate-s3"
@@ -93,9 +90,9 @@ resource "aws_lambda_permission" "allow_s3" {
   source_arn    = aws_s3_bucket.learning-aws-etl.arn
 }
 
-
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.learning-aws-etl.id
+  eventbridge = true
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.activate-s3.arn
@@ -111,8 +108,8 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     aws_lambda_permission.allow_s3
   ]
 }
+
 ########### GLUE ###########
-# Role do Glue
 resource "aws_iam_role" "glue_job_role" {
   name = "glue_job_role"
 
@@ -139,6 +136,12 @@ resource "aws_iam_role_policy_attachment" "glue_service_role" {
   role       = aws_iam_role.glue_job_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
+
+/*resource "aws_s3_object" "python_shell_script" {
+  bucket = var.bucket_name
+  key    = "glue_jobs/glue_job.py"
+  source = "glue/glue_job.py" 
+}*/
 
 resource "aws_glue_job" "python_shell_job" {
   name         = "example-python-shell-job"
@@ -171,8 +174,115 @@ resource "aws_glue_job" "python_shell_job" {
   }
 }
 
-resource "aws_s3_object" "python_shell_script" {
-  bucket = var.bucket_name
-  key    = "glue_jobs/glue_job.py"
-  source = "glue/glue_job.py" # Make sure this file exists locally
+########### SNS ###########
+module "sns_topic" {
+  source  = "terraform-aws-modules/sns/aws"
+
+  name  = "s3-glue-s3-notif"
+  display_name = "AWS SNS"
+
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = module.sns_topic.topic_arn
+  protocol  = "email"
+  endpoint  = var.notification_email
+}
+
+############# EventBridge ###########
+
+resource "aws_cloudwatch_log_group" "example" {
+  name = "/aws/events/s3-create-logs"
+}
+
+resource "aws_cloudwatch_log_resource_policy" "eventbridge_logs_policy" {
+  policy_name = "AllowEventBridgeToWriteToLogs"
+
+  policy_document = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowEventBridgeToPutEvents"
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "${aws_cloudwatch_log_group.example.arn}:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "eventbridge_sns_role" {
+  name = "Amazon_EventBridge_Invoke_Sns_for_s3"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "eventbridge_sns_policy" {
+  name = "EventBridgePublishToSNS"
+  role = aws_iam_role.eventbridge_sns_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "sns:Publish"
+        Effect   = "Allow"
+        Resource = module.sns_topic.topic_arn
+      }
+    ]
+  })
+}
+
+module "eventbridge" {
+  source = "terraform-aws-modules/eventbridge/aws"
+
+  create_bus = false
+
+  targets = {
+    s3-create = [
+      {
+        name            = "SNS topic"
+        arn             = module.sns_topic.topic_arn
+        attach_role_arn = false
+        role_arn        = aws_iam_role.eventbridge_sns_role.arn
+      },
+      {
+        name            = "CloudWatch Log Group"
+        arn             = aws_cloudwatch_log_group.example.arn
+        attach_role_arn = false
+      }
+    ]
+  }
+
+  rules = {
+    s3-create = {
+      description   = "Capture log data"
+      event_pattern = jsonencode({
+        "source"      = ["aws.s3"],
+        "detail-type" = ["Object Created"],
+        "detail" = {
+          "bucket" = {
+            "name" = [var.bucket_name]
+          }
+        }
+      })
+    }
+  }
 }
